410b03cea2bed85f12bff179e4fea37f99fcb5e1
Elaine Froehlich <elaine.froehlich@gmail.com>
2017-11-21 02:41:10 +0000

    working hotelComplexObjectTemplate final html

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,11 @@ var hotel = {
             name: "Luxery Suite",
             price: "$250.00",
             available: 5
+        },
+        {
+            name: "Penthouse with Pool",
+            price: "$6.00",
+            available: 2
         }
     ],
     name: "CareerDevs Innt"
@@ -22,15 +27,46 @@ var hotel = {
 
 //console.log(document.getElementById("radio").nodeName);
 //console.log(document.getElementById("label").nodeName);
+function displayDetails(room) {
+    console.log(room);
+    //display price and availability
+    document.getElementById("price").innerHTML = hotel.rooms[room].price;
+    document.getElementById("available").innerHTML = hotel.rooms[room].available;
+}
 
 for (var i = 0; i < hotel.rooms.length; i++) {
+    console.log("start");
     var radioBtn = document.createElement("INPUT");
     radioBtn.setAttribute("type", "radio");
     radioBtn.setAttribute("name", "roomType");
     radioBtn.setAttribute("value", i);
     radioBtn.setAttribute("id", "room", + i);
-    var radioLbl = document.createElement("LABEL")
+    radioBtn.setAttribute("onclick", "displayDetails(" + i +")");
+    //radioBtn.onclick = displayDetails(i); 
+    var radioLbl = document.createElement("LABEL");
     radioLbl.innerHTML = hotel.rooms[i].name;
     document.getElementById("radialSection").appendChild(radioBtn);
     document.getElementById("radialSection").appendChild(radioLbl);
+    console.log("stop");
+}
+document.getElementById("reservationForm").onsubmit = function(event) {
+    event.preventDefault();
+    if (!document.getElementById("confirmation").checked) {
+        alert("Please agree to terms");
+        return;
+    }
+var radios = document.getElementsByName("roomType");
+var roomSelected = "";
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            roomSelected = radios[i].value;
+            break;
+        }
+    }
+    
+    if(roomSelected == "") {
+        alert("Pick a room please.");
+        return;
+    }
+    alert("You have reserved room " + hotel.rooms[parseInt(roomSelected)].name + ".")
 }
